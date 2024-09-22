ab4e8418bb6c4a759838afaf1c3338bb1281d437
Hubert Miś <hubert.mis@gmail.com>
2024-09-22 21:09:57 +0200

    Remove stale discovered services

--- src/service_discovery.rs
+++ src/service_discovery.rs
@@ -8,7 +8,6 @@ use tokio::time::{sleep, Duration};
 use home_mng::Coap;
 
 struct ProxyEntry {
-    #[allow(dead_code)] // TODO: implement periodic removal of outdated items
     update_timestamp: SystemTime,
     service_type: Option<String>,
     address: SocketAddr,
@@ -19,13 +18,23 @@ pub struct Proxy {
 }
 
 impl Proxy {
+    const DISCOVERY_PERIOD: Duration = Duration::from_secs(600);
+    const CLEANUP_PERIOD: Duration = Self::DISCOVERY_PERIOD;
+    const CLEANUP_INITIAL_DELAY: Duration = Duration::from_secs(30);
+    const CLEANUP_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(3600);
+
     pub fn new() -> Self {
         Self {
             services: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    pub async fn run(&self) {
+    pub async fn run(&'static self) {
+        tokio::spawn(self.discovery_thread());
+        tokio::spawn(self.cleanup_thread());
+    }
+
+    async fn discovery_thread(&self) {
         let coap = Coap::new();
 
         loop {
@@ -39,7 +48,20 @@ impl Proxy {
                 });
             }
 
-            sleep(Duration::from_secs(600)).await;
+            sleep(Self::DISCOVERY_PERIOD).await;
+        }
+    }
+
+    async fn cleanup_thread(&self) {
+        sleep(Self::CLEANUP_INITIAL_DELAY).await;
+
+        loop {
+            self.services.lock().unwrap().retain(
+                    |_, v| v.update_timestamp.elapsed()
+                            .unwrap_or(std::time::Duration::ZERO)
+                            < Self::CLEANUP_TIMEOUT
+                );
+            sleep(Self::CLEANUP_PERIOD).await;
         }
     }
 
